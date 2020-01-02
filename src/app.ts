c8ec80291dc39cf19ce0df0909a368b0c88e3e10
Moshe Gelberg <glbrgm@gmail.com>
2020-01-02 18:12:37 +0200

    Google Maps App

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,37 @@
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address') as HTMLInputElement;
+
+const GOOGLE_API_KEY = '';
+
+type GoogleGeoCodingResponse = {
+  results: { geometry: { location: { lat: number, lng: number } } }[],
+  status: 'OK' | 'ZERO_RESULTS'
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const address = encodeURI(addressInput.value);
+
+  axios.get<GoogleGeoCodingResponse>(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${ address }&key=${ GOOGLE_API_KEY }`
+  )
+    .then(response => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map')!, {
+        center: coordinates,
+        zoom: 16
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch(error => {
+      console.log(error);
+      alert(error.message);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
